a79a3171296eb7813df8a8a5ca9f6a7cbeeb123e
Benny Hartnett <80306702+bennyhartnett@users.noreply.github.com>
2026-04-22 01:30:33 -0400

    Add panoramic subdomain viewer page with loop toggle

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for SWU Calculator PWA
 // Version must be updated when deploying new code to bust cache
-const CACHE_VERSION = 'v136';
+const CACHE_VERSION = 'v137';
 const CACHE_NAME = `swu-calculator-${CACHE_VERSION}`;
 
 // Files to cache for offline use
@@ -19,6 +19,7 @@ const STATIC_ASSETS = [
   '/pages/chat.html',
   '/pages/meet.html',
   '/pages/tools.html',
+  '/pages/panoramic.html',
   '/pages/fx.html',
   '/pages/clipboard.html',
   '/pages/search.html',
